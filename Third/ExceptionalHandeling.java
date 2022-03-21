391cd7865450e8c92bc87c9a7b0bd9b3b2e446a3
Jay Sanodiya <jay.sanodiya09@gmail.com>
2022-03-21 10:59:15 +0530

    Evalluation 03

---
+++ Third/ExceptionalHandeling.java
@@ -0,0 +1,22 @@
+package com.Third;
+
+public class ExceptionalHandeling {
+    /*
+   Ques 1 : What is the difference between Checked and Unchecked Exceptions in Java, create a user-defined checked and unchecked exception class?
+
+   answer :
+   checked exception occurs in compile time while uncheked exception occur at run time..
+
+
+   Ques 2 : What is the difference between the throw and throws keyword in Java?
+
+   answer :
+   throw keyword use to check the exception created by user while throws keyword use to tell the parent class that this class is having exception .
+
+
+  Ques 3 :What is the difference between final and finally in Java?
+
+  answer :
+  finally keyword is used with try catch block, whether try works or not,  catch works or not, if we are using finally then it will definatly works.
+    */
+}
