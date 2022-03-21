058f2dec0293319389ad163e56b8588848e32b4a
Jay Sanodiya <jay.sanodiya09@gmail.com>
2022-03-21 10:45:15 +0530

    Evalluation 03

---
+++ Third/Question03/PlayList.java
@@ -0,0 +1,41 @@
+package com.Third.Question03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PlayList {
+    public List<Song> songs = new ArrayList<>();
+
+    public void addSong(Song song){
+        Song sc = new Song();
+        for (Song s:songs) {
+            if(song.equals(s)){
+                System.out.println("Song is already added in the playlist");
+                return;
+            }
+        }
+        songs.add(song);
+
+        System.out.println("Song added to the playlist successfully.");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        PlayList pl = new PlayList();
+        System.out.println("Enter sons details");
+        int count =0;
+        while(count<5){
+            count++;
+            System.out.println("Enter Song name");
+            String songName = sc.next();
+
+            System.out.println("Enter Movie name");
+            String movieName = sc.next();
+
+            Song song = new Song(songName,movieName);
+            pl.addSong(song);
+        }
+
+    }
+}
---
+++ Third/Question03/Song.java
@@ -0,0 +1,41 @@
+package com.Third.Question03;
+
+import java.util.Objects;
+
+public class Song {
+    private String movieName;
+    private String songName;
+
+    public Song(String movieName, String songName) {
+        this.movieName = movieName;
+        this.songName = songName;
+    }
+
+    public Song() {
+    }
+
+    public void play(){
+        System.out.println(songName + " of " + movieName + " is playing...");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(movieName, song.movieName) && Objects.equals(songName, song.songName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movieName, songName);
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "movieName='" + movieName + '\'' +
+                ", songName='" + songName + '\'' +
+                '}';
+    }
+}
